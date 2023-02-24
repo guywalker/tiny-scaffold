92e619b277dad8ae68344b45cf4335ca538946dd
Guy Walker <guy.w.walker@gmail.com>
2023-02-24 16:53:19 +0000

    Commit dist

---
+++ dist/tiny-scaffold.iife.js
@@ -0,0 +1,59 @@
+var tinyScaffold = function(fs2) {
+  "use strict";
+  function scaffolder(scaffoldConfig, replacements) {
+    return new Promise((resolve, reject) => {
+      try {
+        let config = !Array.isArray(scaffoldConfig) ? [scaffoldConfig] : scaffoldConfig;
+        const filesUpdated = [];
+        config.forEach((template) => {
+          for (let [templateSrc, templateConfig] of Object.entries(template)) {
+            if (typeof templateConfig === "string") {
+              templateConfig = { target: templateConfig };
+            }
+            const { target, selector, insertBefore } = templateConfig;
+            const compiled = compileTemplate(templateSrc, replacements);
+            updateTarget(target, compiled, selector, insertBefore);
+            filesUpdated.push(target);
+          }
+        });
+        resolve(filesUpdated);
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
+  function compileTemplate(templateSrc, replacements) {
+    const templateContent = fs2.readFileSync(templateSrc, "utf8");
+    return replacements ? replaceTokens(templateContent, replacements) : templateContent;
+  }
+  function updateTarget(target, compiled, selector, insertBefore) {
+    const newContent = getNewTargetContent(target, compiled, selector, insertBefore);
+    fs2.writeFileSync(target, newContent, { encoding: "utf8" });
+  }
+  function replaceTokens(templateContent, replacements) {
+    for (const [key, value] of Object.entries(replacements)) {
+      templateContent = templateContent.replace(new RegExp(key, "gm"), value);
+    }
+    return templateContent;
+  }
+  function escapeRegex(string) {
+    return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
+  }
+  function getNewTargetContent(target, templateContent, selector, insertBefore) {
+    if (selector) {
+      if (fs2.existsSync(target)) {
+        const targetContent = fs2.readFileSync(target, "utf8");
+        return targetContent.replace(new RegExp(".*?" + escapeRegex(selector) + ".*", "gm"), (match) => {
+          return insertBefore ? `${templateContent}
+${match}` : `${match}
+${templateContent}`;
+        });
+      } else {
+        throw new Error(`Target file ${target} does not exist`);
+      }
+    } else {
+      return templateContent;
+    }
+  }
+  return scaffolder;
+}(fs);
---
+++ dist/tiny-scaffold.mjs
@@ -0,0 +1,59 @@
+import { readFileSync, writeFileSync, existsSync } from "fs";
+function scaffolder(scaffoldConfig, replacements) {
+  return new Promise((resolve, reject) => {
+    try {
+      let config = !Array.isArray(scaffoldConfig) ? [scaffoldConfig] : scaffoldConfig;
+      const filesUpdated = [];
+      config.forEach((template) => {
+        for (let [templateSrc, templateConfig] of Object.entries(template)) {
+          if (typeof templateConfig === "string") {
+            templateConfig = { target: templateConfig };
+          }
+          const { target, selector, insertBefore } = templateConfig;
+          const compiled = compileTemplate(templateSrc, replacements);
+          updateTarget(target, compiled, selector, insertBefore);
+          filesUpdated.push(target);
+        }
+      });
+      resolve(filesUpdated);
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+function compileTemplate(templateSrc, replacements) {
+  const templateContent = readFileSync(templateSrc, "utf8");
+  return replacements ? replaceTokens(templateContent, replacements) : templateContent;
+}
+function updateTarget(target, compiled, selector, insertBefore) {
+  const newContent = getNewTargetContent(target, compiled, selector, insertBefore);
+  writeFileSync(target, newContent, { encoding: "utf8" });
+}
+function replaceTokens(templateContent, replacements) {
+  for (const [key, value] of Object.entries(replacements)) {
+    templateContent = templateContent.replace(new RegExp(key, "gm"), value);
+  }
+  return templateContent;
+}
+function escapeRegex(string) {
+  return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
+}
+function getNewTargetContent(target, templateContent, selector, insertBefore) {
+  if (selector) {
+    if (existsSync(target)) {
+      const targetContent = readFileSync(target, "utf8");
+      return targetContent.replace(new RegExp(".*?" + escapeRegex(selector) + ".*", "gm"), (match) => {
+        return insertBefore ? `${templateContent}
+${match}` : `${match}
+${templateContent}`;
+      });
+    } else {
+      throw new Error(`Target file ${target} does not exist`);
+    }
+  } else {
+    return templateContent;
+  }
+}
+export {
+  scaffolder as default
+};
